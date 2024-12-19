c57071ac8f8d00f162ebf62484aaf71f9de354f2
daveharms <daveharmswebdev@gmail.com>
2024-12-19 05:11:27 -0600

    initial commit before testing

---
+++ src/queries/actors.queries.ts
@@ -0,0 +1,39 @@
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+export const fetchActorsWithFilmCount = async () => {
+  return prisma.actor.findMany({
+    select: {
+      actor_id: true,
+      first_name: true,
+      last_name: true,
+      _count: {
+        select: {
+          film_actor: true, // Counts the number of films associated with the actor
+        },
+      },
+    },
+  });
+};
+
+export const searchActors = async (searchTerm: string) => {
+  return prisma.actor.findMany({
+    where: {
+      OR: [
+        {
+          first_name: {
+            contains: searchTerm, // Partial match for first_name
+            mode: 'insensitive', // Case-insensitive search
+          },
+        },
+        {
+          last_name: {
+            contains: searchTerm, // Partial match for last_name
+            mode: 'insensitive', // Case-insensitive search
+          },
+        },
+      ],
+    },
+  });
+};
